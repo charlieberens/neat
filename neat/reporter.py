5ab61198c2ad9db83759e97a1f45eb335844e4c0
charlieberens <charliejb3@gmail.com>
2023-06-28 08:53:28 -0600

    Removed progress reporting at end of trial

--- neat/reporter.py
+++ neat/reporter.py
@@ -149,4 +149,5 @@ class ProgressReporter:
             self.population.to_file(os.path.join(self.population.config["progress_directory"],"{}-{}".format(self.population.id, self.population.generation)))
 
     def complete(self):
-        self.report(force = True)
+        # self.report(force = True)
+        pass
